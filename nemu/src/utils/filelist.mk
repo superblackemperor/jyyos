f757c77dd58ab61f52a6de4fa19c42e83bcac8a7
tracer-ics2021 <tracer@njuics.org>
2023-02-02 09:09:12 +0800

    >  compile 2007200016    Static hostname: swxgg-virtual-machine          Icon name: computer-vm            Chassis: vm         Machine ID: 6e82f13215e7466bae3dd5d4b08eeaf0            Boot ID: e9e40c75856f4674934dadacc625fe97     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.15.0-58-generic       Architecture: x86-64  09:09:12 up 10:25,  3 users,  load average: 0.02, 0.03, 0.00

--- nemu/src/utils/filelist.mk
+++ nemu/src/utils/filelist.mk
@@ -1,5 +1,5 @@
 ifdef CONFIG_ITRACE
 CXXSRC = src/utils/disasm.cc
-CXXFLAGS += $(shell llvm-config --cxxflags) -fPIE
+CXXFLAGS += $(shell llvm-config-11 --cxxflags) -fPIE
 LIBS += $(shell llvm-config-11 --libs)
 endif
