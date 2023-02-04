3946594ee5dd8554d9a9721c0f22335589b95bb5
superblackemperor <1546684630@qq.com>
2023-02-04 22:29:00 +0800

    给include/cpu/cpu.h添加了void cpu_exec_setp(uint64_t n);还给sdb.c添加了未完成的cmd_si

--- nemu/include/cpu/cpu.h
+++ nemu/include/cpu/cpu.h
@@ -4,5 +4,5 @@
 #include <common.h>
 
 void cpu_exec(uint64_t n);
-
+void cpu_exec_setp(uint64_t n);
 #endif
--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -3,7 +3,7 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "sdb.h"
-
+#include <stdlib.h>
 static int is_batch_mode = false;
 
 void init_regex();
@@ -39,6 +39,8 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args);//single step
+
 static struct {
   const char *name;
   const char *description;
@@ -47,7 +49,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si", "step a single command",cmd_si }
   /* TODO: Add more commands */
 
 };
@@ -75,6 +77,43 @@ static int cmd_help(char *args) {
     printf("Unknown command '%s'\n", arg);
   }
   return 0;
+}
+
+static int cmd_si(char *args){
+	//judge if exit argument
+	char *arg = strtok(NULL," ");
+	int step_num=1;
+	if(arg!=NULL){
+	char *end;
+	double tmp=strtod(arg,&end);
+	if(tmp-(int)tmp!=0){
+	printf("the type of argument must be int\n");
+	return 0;
+	}else if(tmp==0){
+		printf("thr argument can not be 0\n");
+		return 0;
+		}else
+		step_num =(int)tmp;	
+	}
+	printf("step %d\n",step_num);
+	//cpu exe step_numn
+	
+	Decode s;
+		
+	switch (nemu_state.state) {
+	case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
+	case NEMU_END: case NEMU_ABORT:
+	Log("nemu: %s at pc = " FMT_WORD,
+	(nemu_state.state == NEMU_ABORT ? ASNI_FMT("ABORT", ASNI_FG_RED) :
+	(nemu_state.halt_ret == 0 ? ASNI_FMT("HIT GOOD TRAP", ASNI_FG_GREEN) :			 ASNI_FMT("HIT BAD TRAP", ASNI_FG_RED))),
+	   nemu_state.halt_pc);
+	// fall through
+	 case NEMU_QUIT: statistic();
+      	}
+		
+	return 0;
+
+
 }
 
 void sdb_set_batch_mode() {
