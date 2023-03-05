1dc349dc030dbe8f6c7e9c8fa4bc0050a33ac542
superblackemperor <1546684630@qq.com>
2023-03-05 17:27:27 +0800

    成功执行dummy.c

--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -1,7 +1,14 @@
 def_EHelper(jal){
-	if(id_src1->imm>=0)
-	cpu.pc+=id_src1->imm;
-	else
-	cpu.pc-=id_src1->imm;
-  	rtl_li(s,ddest,cpu.pc+4);
+	/*if(id_src1->simm>=0)
+	s->dnpc=cpu.pc+id_src1->imm;
+	else*/
+	s->dnpc=(int64_t)cpu.pc+(int64_t)id_src1->simm;
+	//s->dnpc=cpu.pc-(id_src1->imm&(~0x80000000));
+  	//rtl_li(s,ddest,s->dnpc+4);
+	rtl_li(s,ddest,cpu.pc+4);
+}
+
+def_EHelper(jalr){
+	s->dnpc=(int64_t)*dsrc1+(int64_t)id_src2->simm;
+	rtl_li(s,ddest,s->dnpc+4);
 }
